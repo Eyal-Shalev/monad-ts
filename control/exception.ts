4a2fa40c7929014fa059c5aaa39b112abcf34043
Eyal Halpern Shalev <eyalsh@gmail.com>
2022-07-10 21:06:04 +0300

    clean

--- control/exception.ts
+++
@@ -1,5 +0,0 @@
-import { Either } from "../data/either.ts";
-import { IO } from "../runner/io.ts";
-
-export interface Exception<TVal, TError extends Error> extends IO<TVal> {
-}
--- internal/guards.ts
+++
@@ -1,6 +0,0 @@
-import { UnreachableError } from "./errors.ts";
-
-/** @internal */
-export function unreachable(): never {
-  throw new UnreachableError();
-}
