cd09746a9d7f0ce37f1207bc631a26ccbf691ff1
Eyal Halpern Shalev <eyalsh@gmail.com>
2023-06-13 22:14:27 +0300

    removed foo.ts

--- foo.ts
+++
@@ -1,6 +0,0 @@
-import { $getEnv } from "./pkg/io/io.ts";
-import { $addEventListener, HasAddEventListener, HasAlert } from "./pkg/io/window.ts";
-
-$getEnv<HasAlert & HasAddEventListener>()
-	.bind(({ alert }) => $addEventListener("error", (ev) => alert(ev.error)))
-	.run(globalThis);
