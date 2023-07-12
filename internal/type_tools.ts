6a257b23bcc3a66333993d9e06cfba2b65e3fa50
Eyal Halpern Shalev <eyalsh@gmail.com>
2023-07-12 21:16:30 +0300

    Removed async_either

--- internal/type_tools.ts
+++ internal/type_tools.ts
@@ -1,13 +1,6 @@
 export type Decrement = [never, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
 export type Increment = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, never];
 
-export type GetArrayItem<T> = T extends (infer I)[] ? I : never;
-export type AsArray<T> = T & GetArrayItem<T>[];
-
-export type AsArray2<T, TItem> = T extends TItem[] ? T & TItem[] : never;
-
-export type AsString<T> = T extends string ? T & string : never;
-
 export type AsTypedFunc<T, TParam, TReturn> = T extends (_: TParam) => TReturn ? T & ((_: TParam) => TReturn) : never;
 
 export type AsFunc<T> = T extends (_: infer TParam) => infer TReturn ? T & ((_: TParam) => TReturn) : never;
@@ -27,10 +20,6 @@ type FoldParams<TReturn, TArr extends unknown[], max extends number, accumulator
 
 export interface Fold<TVals extends unknown[]> {
 	fold<TReturn>(...fns: FoldParams<TReturn, TVals, 10>): TReturn;
-}
-
-export interface AsyncFold<TVals extends unknown[]> {
-	fold<TReturn>(...fns: FoldParams<TReturn, TVals, 10>): Promise<TReturn>;
 }
 
 export interface Stringer {
--- mod.ts
+++ mod.ts
@@ -1,4 +1,8 @@
 export * as maybe from "./pkg/control/maybe.ts";
 export * as either from "./pkg/control/either.ts";
-export * as asyncEither from "./pkg/control/async_either.ts";
 export * as io from "./pkg/control/io.ts";
+export * as state from "./pkg/control/state.ts";
+
+export * as globalTypes from "./pkg/system/global_types.ts";
+export * as sharedGlobals from "./pkg/system/shared_globals.ts";
+export * as window from "./pkg/system/window.ts";
